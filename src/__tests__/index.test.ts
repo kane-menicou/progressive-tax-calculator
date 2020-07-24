5e7d5c0be318e1d4ff05f1520ca60ad4ea322300
Kane Menicou <kanemenicou@gmail.com>
2020-07-25 00:49:30 +0100

    Tests for other rates and move to generic progressive tax calculator

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,13 @@
-import {calculateTax} from "../index";
+import calculateTax, {uk2020to2021Config} from "../index";
 
 describe('index.ts', () => {
   describe('getIncomeTax()', () => {
-    describe('2020/2021', () => {
+    describe('with uk2020to2021Config', () => {
       it('should get the correct values £20,000', () => {
-        expect(calculateTax(20_000, "2020/2021")).toEqual(
+        expect(calculateTax(20_000, uk2020to2021Config)).toEqual(
           {
-            taxYear: "2020/2021",
-            takeHomePay: 17_242,
-            incomeTaxPaid: 4_018,
+            takeHomePay: '18500.20',
+            incomeTaxPaid: '1499.80',
             incomeTaxBands: [
               {
                 band: "Personal Allowance",
@@ -36,6 +35,196 @@ describe('index.ts', () => {
                 rate: 0.45,
                 taxableFrom: 150_001.00,
                 taxedInBand: '0.00',
+              }
+            ],
+          }
+        );
+      });
+
+      it('should get the correct values £50,000', () => {
+        expect(calculateTax(50_000, uk2020to2021Config)).toEqual(
+          {
+            takeHomePay: '42500.20',
+            incomeTaxPaid: '7499.80',
+            incomeTaxBands: [
+              {
+                band: "Personal Allowance",
+                taxableUpto: 12_500,
+                taxableFrom: 0,
+                rate: 0,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Basic rate",
+                taxableUpto: 50_000.00,
+                taxableFrom: 12_501.00,
+                rate: 0.2,
+                taxedInBand: '7499.80',
+              },
+              {
+                band: "Higher rate",
+                taxableUpto: 150_000,
+                taxableFrom: 50_001.00,
+                rate: 0.4,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Additional rate",
+                rate: 0.45,
+                taxableFrom: 150_001.00,
+                taxedInBand: '0.00',
+              }
+            ],
+          }
+        );
+      });
+
+      it('should get the correct values £44,391.54', () => {
+        expect(calculateTax(44_391.54, uk2020to2021Config)).toEqual(
+          {
+            incomeTaxPaid: "6378.11",
+            takeHomePay: "38013.43",
+            incomeTaxBands: [
+              {
+                band: "Personal Allowance",
+                taxableUpto: 12_500,
+                taxableFrom: 0,
+                rate: 0,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Basic rate",
+                taxableUpto: 50_000.00,
+                taxableFrom: 12_501.00,
+                rate: 0.2,
+                taxedInBand: '6378.11',
+              },
+              {
+                band: "Higher rate",
+                taxableUpto: 150_000,
+                taxableFrom: 50_001.00,
+                rate: 0.4,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Additional rate",
+                rate: 0.45,
+                taxableFrom: 150_001.00,
+                taxedInBand: '0.00',
+              }
+            ],
+          }
+        );
+      });
+
+      it('should get the correct values £60,000', () => {
+        expect(calculateTax(60_000, uk2020to2021Config)).toEqual(
+          {
+            takeHomePay: '48500.60',
+            incomeTaxPaid: '11499.40',
+            incomeTaxBands: [
+              {
+                band: "Personal Allowance",
+                taxableUpto: 12_500,
+                taxableFrom: 0,
+                rate: 0,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Basic rate",
+                taxableUpto: 50_000.00,
+                taxableFrom: 12_501.00,
+                rate: 0.2,
+                taxedInBand: '7499.80',
+              },
+              {
+                band: "Higher rate",
+                taxableUpto: 150_000,
+                taxableFrom: 50_001.00,
+                rate: 0.4,
+                taxedInBand: '3999.60',
+              },
+              {
+                band: "Additional rate",
+                rate: 0.45,
+                taxableFrom: 150_001.00,
+                taxedInBand: '0.00',
+              }
+            ],
+          }
+        );
+      });
+
+      it('should get the correct values £150,000', () => {
+        expect(calculateTax(150_000, uk2020to2021Config)).toEqual(
+          {
+            takeHomePay: '102500.60',
+            incomeTaxPaid: '47499.40',
+            incomeTaxBands: [
+              {
+                band: "Personal Allowance",
+                taxableUpto: 12_500,
+                taxableFrom: 0,
+                rate: 0,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Basic rate",
+                taxableUpto: 50_000.00,
+                taxableFrom: 12_501.00,
+                rate: 0.2,
+                taxedInBand: '7499.80',
+              },
+              {
+                band: "Higher rate",
+                taxableUpto: 150_000,
+                taxableFrom: 50_001.00,
+                rate: 0.4,
+                taxedInBand: '39999.60',
+              },
+              {
+                band: "Additional rate",
+                rate: 0.45,
+                taxableFrom: 150_001.00,
+                taxedInBand: '0.00',
+              }
+            ],
+          }
+        );
+      });
+
+      it('should get the correct values £200,000', () => {
+        expect(calculateTax(200_000, uk2020to2021Config)).toEqual(
+          {
+            takeHomePay: '130001.05',
+            incomeTaxPaid: '69998.95',
+            incomeTaxBands: [
+              {
+                band: "Personal Allowance",
+                taxableUpto: 12_500,
+                taxableFrom: 0,
+                rate: 0,
+                taxedInBand: '0.00',
+              },
+              {
+                band: "Basic rate",
+                taxableUpto: 50_000.00,
+                taxableFrom: 12_501.00,
+                rate: 0.2,
+                taxedInBand: '7499.80',
+              },
+              {
+                band: "Higher rate",
+                taxableUpto: 150_000,
+                taxableFrom: 50_001.00,
+                rate: 0.4,
+                taxedInBand: '39999.60',
+              },
+              {
+                band: "Additional rate",
+                rate: 0.45,
+                taxableFrom: 150_001.00,
+                taxedInBand: '22499.55',
               }
             ],
           }
--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,85 @@
+interface TaxDetailsBand {
+  band: string,
+  taxableUpto?: number,
+  taxableFrom: number,
+  rate: number,
+  taxedInBand: string,
+}
+
 interface TaxDetails {
-  taxYear: string,
-  takeHomePay: number,
-  incomeTaxPaid: number,
-  incomeTaxBands: {
+  takeHomePay: string,
+  incomeTaxPaid: string,
+  incomeTaxBands: TaxDetailsBand[]
+}
+
+interface TaxConfig {
+  bands: {
     band: string,
     taxableUpto?: number,
     taxableFrom: number,
     rate: number,
-    taxedInBand: string,
   }[]
 }
 
-export function calculateTax(net: number, taxYear: string): TaxDetails {
+export const uk2020to2021Config: TaxConfig = {
+  bands: [
+    {
+      band: "Personal Allowance",
+      taxableUpto: 12500,
+      taxableFrom: 0,
+      rate: 0,
+    },
+    {
+      band: "Basic rate",
+      taxableUpto: 50_000.00,
+      taxableFrom: 12_501.00,
+      rate: 0.2,
+    },
+    {
+      band: "Higher rate",
+      taxableUpto: 150_000.00,
+      taxableFrom: 50_001.00,
+      rate: 0.4,
+    },
+    {
+      band: "Additional rate",
+      rate: 0.45,
+      taxableFrom: 150_001.00,
+    }
+  ]
+};
+
+export default function calculateTax(net: number, config: TaxConfig): TaxDetails {
+  const taxedInBands = config.bands.map(
+    ({rate, taxableFrom, taxableUpto}): number => calculateTaxPaid(net, rate, taxableFrom, taxableUpto)
+  );
+
+  const toSumOfValues = (total: number, current: number): number => total + current;
+
+  const incomeTaxPaid = taxedInBands.reduce(toSumOfValues, 0);
+
+  const takeHomePay = formatCurrency(net - incomeTaxPaid);
+
   return {
-    taxYear,
-    takeHomePay: 17242,
-    incomeTaxPaid: 4018,
-    incomeTaxBands: [
-      {
-        band: "Personal Allowance",
-        taxableUpto: 12500,
-        taxableFrom: 0,
-        rate: 0,
-        taxedInBand: calculateTaxPaid(net, 0, 0, 12500),
-      },
-      {
-        band: "Basic rate",
-        taxableUpto: 50_000.00,
-        taxableFrom: 12_501.00,
-        rate: 0.2,
-        taxedInBand: calculateTaxPaid(net, 0.2, 12_501.00, 50_000.00),
-      },
-      {
-        band: "Higher rate",
-        taxableUpto: 150_000.00,
-        taxableFrom: 50_001.00,
-        rate: 0.4,
-        taxedInBand: '0.00',
-      },
-      {
-        band: "Additional rate",
-        rate: 0.45,
-        taxableFrom: 150_001.00,
-        taxedInBand: '0.00',
-      }
-    ],
+    takeHomePay,
+    incomeTaxPaid: formatCurrency(incomeTaxPaid),
+    incomeTaxBands: config.bands.map((config, index): TaxDetailsBand => ({
+      ...config,
+      taxedInBand: formatCurrency(taxedInBands[index])
+    }))
   };
 }
 
-function calculateTaxPaid(net: number, rate: number, from: number, to: number = net): string {
+function calculateTaxPaid(net: number, rate: number, from: number, to: number = net): number {
   const taxableIncome = Math.min(net, to) - from;
 
-  return (taxableIncome * rate).toFixed(2)
+  if (taxableIncome < 0) {
+    return 0;
+  }
+
+  return taxableIncome * rate;
+}
+
+function formatCurrency(number: number): string {
+  return number.toFixed(2);
 }
